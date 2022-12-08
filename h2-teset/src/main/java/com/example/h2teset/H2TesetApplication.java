20c73b037c312a2c9f98f4a4827bbf0899d563e8
jjjjooo <fullbster@gmail.com>
2022-12-09 00:18:16 +0900

    h2-test 설정

---
+++ h2-teset/src/main/java/com/example/h2teset/H2TesetApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2teset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2TesetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2TesetApplication.class, args);
+    }
+
+}
